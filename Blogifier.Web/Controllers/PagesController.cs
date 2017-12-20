35b4a3f8ea3525224311d4a768b10cd1a16b1f9f
Lars Richter <lars.richter@neveling.net>
2017-12-20 09:39:33 +0100

    Added views and controller for pages

---
+++ Blogifier.Web/Controllers/PagesController.cs
@@ -0,0 +1,26 @@
+﻿using Blogifier.Core.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogifier.Controllers
+{
+    public class PagesController : Controller
+    {
+        private readonly string _theme;
+
+        public PagesController()
+        {
+            _theme = $"~/{ApplicationSettings.BlogThemesFolder}/{BlogSettings.Theme}/";
+
+        }
+
+        public IActionResult About()
+        {
+            return View(_theme + "Pages/About.cshtml");
+        }
+
+        public IActionResult Imprint()
+        {
+            return View(_theme + "Pages/Imprint.cshtml");
+        }
+    }
+}
